7f004672952424c62cee4096728a23d102dae693
Tuandicodedao <91586684+NguyenDucTuan404@users.noreply.github.com>
2023-07-28 17:55:50 +0700

    Install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -21,21 +21,19 @@ app.use(express.json());
 app.use(morgan("combined"));
 
 // Template engine
-            app.engine(
+app.engine(
   "hbs",
   handlebars.engine({
     extname: ".hbs",
   })
 );
-        app.set("view engine", "hbs");
+app.set("view engine", "hbs");
 
 app.set("views", path.join(__dirname, "resources", "views"));
 
 //route init
-        route(app);
+route(app);
 
-
-        
-        app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
 });
